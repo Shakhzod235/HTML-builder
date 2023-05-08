0195876c007b6f4609e5a5a2766cbffb489916e5
Shakhzod235 <shakhzod235@mail.ru>
2023-05-09 01:03:22 +0500

    feat: add solution for 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(directoryPath, (err, files) => {
+  if (err) {
+    console.log(`Невозможно прочитать директорию: ${err}`);
+  } else if (files.length === 0) {
+      console.log('Директория пуста');
+  } else {
+      files.forEach((file) => {
+        const filePath = path.join(directoryPath, file);
+        fs.stat(filePath, (error, stats) => {
+          if (error) {
+            console.log(`Невозможно получить информацию о файлах: ${error}`);
+          } else if (stats.isFile()) {
+            const fileSizeInBytes = stats.size;
+            const fileSizeInKB = fileSizeInBytes / 1024;
+            const fileExtension = path.extname(file);
+            const fileName = path.basename(file, fileExtension);
+            console.log(`${fileName} - ${fileExtension.slice(1)} - ${fileSizeInKB}kb`);
+          }
+        });
+      });
+    }
+});
