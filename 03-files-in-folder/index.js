1334d859e36ff73d826a4195a7a88f526e6371c5
Shakhzod235 <shakhzod235@mail.ru>
2023-05-09 12:01:20 +0500

    refactor: fix indents

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,21 +7,21 @@ fs.readdir(directoryPath, (err, files) => {
   if (err) {
     console.log(`Невозможно прочитать директорию: ${err}`);
   } else if (files.length === 0) {
-      console.log('Директория пуста');
+    console.log('Директория пуста');
   } else {
-      files.forEach((file) => {
-        const filePath = path.join(directoryPath, file);
-        fs.stat(filePath, (error, stats) => {
-          if (error) {
-            console.log(`Невозможно получить информацию о файлах: ${error}`);
-          } else if (stats.isFile()) {
-            const fileSizeInBytes = stats.size;
-            const fileSizeInKB = fileSizeInBytes / 1024;
-            const fileExtension = path.extname(file);
-            const fileName = path.basename(file, fileExtension);
-            console.log(`${fileName} - ${fileExtension.slice(1)} - ${fileSizeInKB}kb`);
-          }
-        });
+    files.forEach((file) => {
+      const filePath = path.join(directoryPath, file);
+      fs.stat(filePath, (error, stats) => {
+        if (error) {
+          console.log(`Невозможно получить информацию о файлах: ${error}`);
+        } else if (stats.isFile()) {
+          const fileSizeInBytes = stats.size;
+          const fileSizeInKB = fileSizeInBytes / 1024;
+          const fileExtension = path.extname(file);
+          const fileName = path.basename(file, fileExtension);
+          console.log(`${fileName} - ${fileExtension.slice(1)} - ${fileSizeInKB}kb`);
+        }
       });
-    }
+    });
+  }
 });
