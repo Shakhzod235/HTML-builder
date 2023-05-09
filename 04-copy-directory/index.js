10c00294b7c7b3fa792c00c2c756a32d66e8a7b3
Shakhzod235 <shakhzod235@mail.ru>
2023-05-09 12:59:24 +0500

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const src = path.join(__dirname, 'files');
+const dest = path.join(__dirname,'files-copy');
+
+fs.mkdir(dest, { recursive: true }, (err) => {
+  if (err) throw err;
+  fs.readdir(src, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      const srcPath = path.join(src, file);
+      const destPath = path.join(dest, file);
+      fs.stat(srcPath, (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile()) {
+          fs.readFile(srcPath, (err, data) => {
+            if (err) throw err;
+            fs.writeFile(destPath, data, (err) => {
+              if (err) throw err;
+            });
+          });
+        }
+      });
+    });
+  });
+});
